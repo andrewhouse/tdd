803ee673a3a06d7408854695c33f959979e26ddb
Andrew House <andrewhouse621@gmail.com>
2014-05-22 13:36:12 -0400

    finished tests

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ActiveRecord::Base
   attr_accessible :note_text
   belongs_to :person
+
+  
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,20 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :comapny_name, :email
-  
   has_many :notes
 
-	
+  
+	def self.merge(winner, loser)
+  	winner.email << loser.email if winner.email.empty?  
+    winner.notes << loser.notes
+    loser.destroy
+    winner
+  end
+
+  def duplicates?(person)
+    return false if person == nil
+		person_attrs = person.first_name, person.last_name, person.email
+		self_attrs = self.first_name, self.last_name, self.email
+		person_attrs == self_attrs    
+  end
+
 end
